74293cd6ddd6ce2e6ea5e259e8f17cd7309b9e8d
Magee_Yang <magee_yang@126.com>
2015-08-24 17:37:35 +0800

    1

---
+++ src/pers/magee/emap/sdp/dispatcher/http/DispatcherServlet.java
@@ -0,0 +1,27 @@
+package pers.magee.emap.sdp.dispatcher.http;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DispatcherServlet extends HttpServlet {
+
+	/**
+	 * 该Servlet用于处理所有的HTTP请求，并进行请求内容的路由分配处理
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request,response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("this is doPost");
+	}
+
+}
